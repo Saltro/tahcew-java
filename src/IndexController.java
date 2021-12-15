e197ddd88e58491d22d281d909087ee082792028
saltro <2373117747@qq.com>
2021-12-15 20:51:50 +0800

    feat: 增加右侧聊天室列表，方便直接跳转

--- src/IndexController.java
+++ src/IndexController.java
@@ -1,9 +1,12 @@
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import network.Session;
 import tasks.ReceiveTask;
 import utils.Log;
@@ -23,6 +26,9 @@ public class IndexController {
     @FXML
     private TextArea inputArea;
 
+    @FXML
+    private VBox roomList;
+
     public void setSession(Session session) {
         this.session = session;
     }
@@ -34,9 +40,10 @@ public class IndexController {
     @FXML
     private void onRoomInput() {
         String roomName = roomInput.getText();
-        if (roomName.length() > 0 && session.sendMessage("join/" + roomName)) {
-            roomInput.setText("");
+        if (roomName.length() > 0) {
+            changeRoom(roomName);
         }
+        roomInput.setText("");
     }
 
     @FXML
@@ -56,6 +63,7 @@ public class IndexController {
                 break;
             case "room":
                 showRoomTitle(args[1]);
+                addRoomNameIntoList(args[1]);
         }
     }
 
@@ -65,5 +73,28 @@ public class IndexController {
 
     private void showRoomTitle(String title) {
         roomTitle.setText(title);
+    }
+
+    private void changeRoom(String roomName) {
+        if (!roomName.equals(roomTitle.getText())) {
+            session.sendMessage("join/" + roomName);
+        }
+    }
+
+    private void addRoomNameIntoList(String name) {
+        for (Node node: roomList.getChildren()) {
+            if (node instanceof HBox && ((Label) ((HBox) node).getChildren().get(0)).getText().equals(name)) {
+                return;
+            }
+        }
+
+        HBox box = new HBox();
+        box.setStyle("-fx-padding: 5 12; -fx-border-width: 0 0 1 0; -fx-border-color: #E0E0E0;");
+        box.setOnMouseClicked((e) -> changeRoom(name));
+
+        Label room = new Label(name);
+        box.getChildren().add(room);
+
+        roomList.getChildren().add(box);
     }
 }
